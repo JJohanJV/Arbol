113a96e292d13e485e405625671ae491e1ac3bd7
JJohanJV <99496316+JJohanJV@users.noreply.github.com>
2022-08-31 00:39:39 -0500

    Update Implementacion arbol.cpp

--- Implementacion arbol.cpp
+++ Implementacion arbol.cpp
@@ -1,61 +1,195 @@
 #include <iostream>
 using namespace std;
+const int orden = 6;
 
 struct Nodo
 {
     int dato;
-    Nodo* hijos[6];
+    Nodo* hijos[orden];
   
 };
 
-void agregar(Nodo*& raiz, int dato) {
+void agregarRaiz(Nodo*& raiz, int dato) {
 
     Nodo* nuevoNodo = new Nodo;
     nuevoNodo->dato = dato;
 
     raiz = nuevoNodo;
+
+	for(int i=0; i<orden; i++){
+		raiz->hijos[i]=NULL;
+	}
 }
 
-void agregar(Nodo*& raiz, int dato, int padre) {
-
-    Nodo* actual = raiz;
-
+int contadorHijos(Nodo*& nodo){
+	int contador=0;
+	for(int i=0; i<orden; i++){
+		if(nodo->hijos[i] != NULL){
+			contador++;
+		}
+	}
+	return contador;
 }
 
+int contadorHijos(Nodo*& raiz, int datoDelNodo){ //Retorna el numero de hijos del nodo con el dato dado
+	
+	if(raiz->dato == datoDelNodo){
+		int contador=0;
+		for(int i=0; i<orden; i++){
+			if(raiz->hijos[i] != NULL){
+				contador++;
+			}
+		}
+		return contador;
+	}else{
+		if(raiz->hijos>0){
+			for(int i=0; i<contadorHijos(raiz); i++){
+				if(raiz->hijos[i]->dato == datoDelNodo){
+					int contador=0;
+					for(int i=0; i<orden; i++){
+						if(raiz->hijos[i] != NULL){
+							contador++;
+						}
+					}
+					return contador;
+					break;
+				}else{
+	    			contadorHijos(raiz->hijos[i], datoDelNodo);
+				}
+			}
+		}
+	}
+}
+
+void buscarNodo(Nodo*& raiz, int datoDelNodo){ //Busca el dato dado y si está lo imprime
+	
+	if(raiz->dato == datoDelNodo){
+		cout << "Encontre el nodo " << raiz->dato << "\n";
+	}else{
+		if(raiz->hijos>0){
+			for(int i=0; i<contadorHijos(raiz); i++){
+				if(raiz->hijos[i]->dato == datoDelNodo){
+					cout << "Encontre el nodo " << raiz->hijos[i]->dato << "\n";
+					break;
+				}else{
+	    			buscarNodo(raiz->hijos[i], datoDelNodo);
+				}
+			}
+		}
+	}
+}
+
+void agregarNodo(Nodo*& raiz, int padre, int dato){ //Retorna el padre del dato dado
+	
+	Nodo* actual;
+
+	if(raiz->dato == padre){
+		cout << "Se encontro el padre " << raiz->dato;
+		actual=raiz;
+		Nodo* nuevoNodo = new Nodo;
+		nuevoNodo->dato = dato;
+		for(int i=0; i<6; i++){
+			nuevoNodo->hijos[i]=NULL;
+		}
+	
+		actual->hijos[contadorHijos(actual)] = nuevoNodo;
+		cout << " y se agrego el nodo "<< actual->hijos[contadorHijos(actual)-1]->dato << "\n";
+	}else{
+		if(raiz->hijos>0){
+			for(int i=0; i<contadorHijos(raiz); i++){
+				if(raiz->hijos[i]->dato == padre){
+					cout << "Se encontro el padre " << raiz->hijos[i]->dato;
+					actual = raiz->hijos[i];
+					Nodo* nuevoNodo = new Nodo;
+					nuevoNodo->dato = dato;
+					for(int i=0; i<6; i++){
+						nuevoNodo->hijos[i]=NULL;
+					}
+					actual->hijos[contadorHijos(actual)] = nuevoNodo;
+					cout << " y agrego el nuevo nodo "<< actual->hijos[contadorHijos(actual)-1]->dato << "\n";
+					break;
+				}else{
+	    			agregarNodo(raiz->hijos[i], padre, dato);
+				}
+			}
+		}
+	}
+}
+
+
+
+void agregar(Nodo*& raiz){
+	
+	int hijos;
+	int dato;
+	
+	cout << "Ingrese cuantos hijos tiene el nodo ->";
+	cin >> hijos;
+	if(hijos>0){
+		for(int i=0; i<hijos; i++){
+			cout << "Ingrese el valor del hijo " << i+1 << " del nodo con valor" << raiz->dato << " ->";  
+			cin >> dato;
+			Nodo* nuevoNodo = new Nodo;
+    		nuevoNodo->dato = dato;
+    		for(int j=0; j<6; j++){
+				nuevoNodo->hijos[j]=NULL;
+			}
+    		raiz->hijos[i] = nuevoNodo;
+    		agregar(raiz->hijos[i]);
+		}
+	}
+	
+}
+
+void imprimirArbol(Nodo*& raiz){
+	
+	cout << raiz->dato << " \n";
+	
+	if(raiz->hijos>0){
+		for(int i=0; i<contadorHijos(raiz); i++){
+			imprimirArbol(raiz->hijos[i]);
+		}
+	}
+	
+}
 
 int main()
 {
     int padre;
-    int hijos;
     int dato;
     bool continuar = true;
     Nodo* raiz = NULL;
     
-
-    cout << "inserte el valor de la raiz ->";
+	
+    cout << "Inserte el valor de la raiz ->";
     cin >> dato;
-    agregar(raiz, dato);
+    agregarRaiz(raiz, dato);
 
-    while(continuar){
-        cout << "Ingrese 1 para agregar un nuevo nodo al arbol o 0 para terminar arbol ->";
-        cin >> dato;
-        switch (dato)
-        {
-        case 1: 
-            cout << "Ingrese el valor del padre del nuevo nodo ->";
-            cin >> dato;
-
-        }
-    }
+	while(continuar){
+		cout << "\nIngrese 1 para agregar un nodo al arbol, 2 para ver el arbol actual, 3 para salir ->";
+		cin>>dato;
+		switch(dato){
+			case 1:{
+				cout << "\nIngrese el padre del nuevo nodo (Asegurese de que el padre este en el arbol)->";
+				cin >> padre;
+				cout << "Ingrese el valor del nuevo nodo ->";
+				cin >> dato;
+				agregarNodo(raiz, padre, dato);
+				break;
+			}
+			
+			case 2:{
+				cout << "\nObservacion: El arbol se representa por ramas, es decir se parte desde la raiz hasta el final del primer hijo y luego se vuelve al segundo hijo de la raiz y así sucesivamente, se debe identificar donde empieza el siguiente hijo.\n";
+				imprimirArbol(raiz);
+				break;
+			}
+			
+			case 3:{
+				continuar = false;
+				cout << "\nPrograma terminado";
+				break;
+			}
+		}
+	}	 
 
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
